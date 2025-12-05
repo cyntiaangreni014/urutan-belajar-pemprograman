7cf7a0ccc313d4437a097e73dd9bb678b0c64ab9
cyntia angreni <chintyaangreni@gmail.com>
2025-12-05 10:15:58 +0800

    Add files via upload

---
+++ 1.Tipe data.py
@@ -0,0 +1,16 @@
+#Nama : Cyntia angreni
+#NIM : D0425315
+#Prodi : Sistem Informasi / Class A
+#Mata kuliah : Algoritma dan struktur data (Pak Muzaki)
+
+#1.1 Tipe data Character,Integer,dan Floating point :
+
+#1.) Tipe data character : 1.Integer 2.Float 3.String 4.Boolean 5.List 6.Tuple 7.Set 8.Dictionary
+
+#2.) Integer dalam Python adalah tipe data untuk bilangan bulat positif atau negatif, seperti 10, -5, atau 10000. Contoh :
+contoh = 10
+print (contoh)
+
+#3.) Dalam Python, " Floating " mengacu pada tipe data float yang digunakan untuk merepresentasikan angka yang memiliki titik desimal atau bilangan pecahan.
+contoh = 1.555
+print (contoh)
